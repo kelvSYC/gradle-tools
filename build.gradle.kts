9a5782bbeb44dc820c1f385f8c3129e28975a8fd
Kelvin Chung <kelvsyc@Mobile-Brain-IV.local>
2025-02-15 15:51:38 -0800

    GitHub workflow for builds

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,23 @@
 group = "com.kelvsyc"
+
+val components = buildList {
+    add("git-core")
+}
+
+tasks.register("clean") {
+    components.forEach {
+        dependsOn(gradle.includedBuild(it).task(":$name"))
+    }
+}
+
+tasks.register("assemble") {
+    components.forEach {
+        dependsOn(gradle.includedBuild(it).task(":$name"))
+    }
+}
+
+tasks.register("build") {
+    components.forEach {
+        dependsOn(gradle.includedBuild(it).task(":$name"))
+    }
+}
