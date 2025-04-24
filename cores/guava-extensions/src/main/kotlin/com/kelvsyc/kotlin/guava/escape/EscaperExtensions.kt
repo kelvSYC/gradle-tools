89775f56b1c877ccc7bcd765698513b9b72c7868
Kelvin Chung <kelv@kelvSYC.com>
2025-04-23 19:20:03 -0700

    [minor](guava-extensions) HashFunctionExtensions

--- cores/guava-extensions/src/main/kotlin/com/kelvsyc/kotlin/guava/escape/EscaperExtensions.kt
+++ cores/guava-extensions/src/main/kotlin/com/kelvsyc/kotlin/guava/escape/EscaperExtensions.kt
@@ -1,8 +1,18 @@
 package com.kelvsyc.kotlin.guava.escape
 
+import com.google.common.escape.Escaper
 import com.google.common.escape.Escapers
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 /**
  * Returns an [Escaper] created from populating an [Escapers.Builder] using the given action.
  */
-fun buildEscaper(action: Escapers.Builder.() -> Unit) = Escapers.builder().apply(action).build()
+@OptIn(ExperimentalContracts::class)
+fun buildEscaper(action: Escapers.Builder.() -> Unit): Escaper {
+    contract {
+        callsInPlace(action, InvocationKind.EXACTLY_ONCE)
+    }
+    return Escapers.builder().apply(action).build()
+}
