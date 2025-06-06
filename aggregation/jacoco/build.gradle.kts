1d4f23290958b4bb6795766836131851d275580c
Kelvin Chung <kelv@kelvSYC.com>
2025-06-06 15:17:21 -0700

    Aggregate JaCoCo report for core components

--- aggregation/jacoco/build.gradle.kts
+++ aggregation/jacoco/build.gradle.kts
@@ -4,11 +4,17 @@ plugins {
 
 group = "com.kelvsyc.gradle"
 
+val coreComponents = gradle.includedBuilds.filter {
+    it.projectDir.parentFile.name == "cores"
+}
 val extensionComponents = gradle.includedBuilds.filter {
     it.projectDir.parentFile.name == "extensions"
 }
 
 dependencies {
+    coreComponents.forEach {
+        jacocoAggregation("$group:${it.name}") // from included build $it.name
+    }
     extensionComponents.forEach {
         jacocoAggregation("$group:${it.name}") // from included build $it.name
     }
