3ec5ec5807c834a18315e4082b0457c0a59cd326
Kelvin Chung <kelvSYC@users.noreply.github.com>
2026-05-12 07:17:00 -0700

    [aws-kms-kotlin-base] Migrate to AbstractClientBuildService (#506)
    Standard Kotlin variant migration. Single KmsClientBuildService (region
    + credentials). Value sources / WorkActions take
    Property<KmsClientBuildService>. Ship as a library; drop the plugin and
    the aws-kotlin-extensions includeBuild.
    
    Drop libs.aws.smithy.client (no longer needed once AwsClientInfo is
    gone). Promote libs.aws.smithy.credentials to api since CredentialsProvider
    appears in Params.

--- cores/aws-kms-kotlin-base/src/main/kotlin/com/kelvsyc/gradle/aws/kotlin/kms/KmsClientInfo.kt
+++
@@ -1,6 +0,0 @@
-package com.kelvsyc.gradle.aws.kotlin.kms
-
-import aws.sdk.kotlin.services.kms.KmsClient
-import com.kelvsyc.gradle.aws.kotlin.AwsClientInfo
-
-interface KmsClientInfo : AwsClientInfo<KmsClient>
--- cores/aws-kms-kotlin-base/src/test/kotlin/com/kelvsyc/gradle/aws/kotlin/kms/DecryptActionSpec.kt
+++ cores/aws-kms-kotlin-base/src/test/kotlin/com/kelvsyc/gradle/aws/kotlin/kms/DecryptActionSpec.kt
@@ -3,28 +3,22 @@ package com.kelvsyc.gradle.aws.kotlin.kms
 import aws.sdk.kotlin.services.kms.KmsClient
 import aws.sdk.kotlin.services.kms.model.DecryptRequest
 import aws.sdk.kotlin.services.kms.model.DecryptResponse
-import com.kelvsyc.gradle.clients.ClientsBaseExtension
-import com.kelvsyc.gradle.internal.aws.kotlin.kms.MockKmsClientInfoInternal
-import com.kelvsyc.gradle.plugins.KmsKotlinBasePlugin
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
+import io.mockk.mockk
 import io.mockk.slot
-import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.newInstance
-import org.gradle.kotlin.dsl.the
+import org.gradle.kotlin.dsl.registerIfAbsent
 import org.gradle.testfixtures.ProjectBuilder
 
 class DecryptActionSpec : FunSpec() {
     init {
         test("execute - sends ciphertext bytes and writes returned plaintext") {
             val project = ProjectBuilder.builder().build()
-            project.pluginManager.apply(KmsKotlinBasePlugin::class)
-            val extension = project.the<ClientsBaseExtension>()
-            extension.service.get().registerBinding(MockKmsClientInfo::class, MockKmsClientInfoInternal::class)
-            extension.service.get().registerIfAbsent<MockKmsClientInfo>("mock") {}
-
-            val client = extension.getClient<KmsClient, MockKmsClientInfo>("mock").get()!!
+            val client = mockk<KmsClient>()
+            MockKmsClientBuildService.mockClient = client
+            val service = project.gradle.sharedServices.registerIfAbsent("kms", MockKmsClientBuildService::class)
             val requestSlot = slot<DecryptRequest>()
             val expectedPlaintext = "hello".toByteArray()
             coEvery { client.decrypt(capture(requestSlot)) } returns DecryptResponse {
@@ -38,8 +32,7 @@ class DecryptActionSpec : FunSpec() {
             val plaintextPath = project.layout.buildDirectory.file("plain.txt").get().asFile
 
             val params = project.objects.newInstance<DecryptAction.Parameters>()
-            params.service.set(extension.service.get())
-            params.clientName.set("mock")
+            params.service.set(service)
             params.ciphertextFile.set(ciphertextPath)
             params.plaintextFile.set(plaintextPath)
 
