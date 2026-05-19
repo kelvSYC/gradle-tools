932ebdf38fe3967e6f68642b6af74456bbf1d160
Kelvin Chung <kelv@kelvSYC.com>
2026-05-18 22:07:50 -0700

    feat: add UpdateFunctionAppSettingsAction with sensitive settings support

---
+++ cores/azure-functions-base/src/test/kotlin/com/kelvsyc/gradle/azure/functions/UpdateFunctionAppSettingsActionSpec.kt
@@ -0,0 +1,62 @@
+package com.kelvsyc.gradle.azure.functions
+
+import com.azure.resourcemanager.appservice.AppServiceManager
+import com.azure.resourcemanager.appservice.models.FunctionApp
+import com.azure.resourcemanager.appservice.models.FunctionApps
+import com.kelvsyc.gradle.clients.CredentialReference
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.maps.shouldContainAll
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.unmockkAll
+import org.gradle.kotlin.dsl.newInstance
+import org.gradle.kotlin.dsl.registerIfAbsent
+import org.gradle.testfixtures.ProjectBuilder
+
+class UpdateFunctionAppSettingsActionSpec : FunSpec() {
+    init {
+        afterTest { unmockkAll() }
+
+        test("execute - merges plain and sensitive settings into a single ARM update call") {
+            val project = ProjectBuilder.builder().build()
+            val mockManager = mockk<AppServiceManager>()
+            val mockFunctionApps = mockk<FunctionApps>()
+            val mockApp = mockk<FunctionApp>()
+            val mockUpdate = mockk<FunctionApp.Update>()
+
+            MockFunctionAppClientBuildService.mockClient = mockManager
+            every { mockManager.functionApps() } returns mockFunctionApps
+            every { mockFunctionApps.getByResourceGroup("test-rg", "my-app") } returns mockApp
+            val capturedSettings = mutableMapOf<String, String>()
+            every { mockApp.update() } returns mockUpdate
+            every { mockUpdate.withAppSetting(any(), any()) } answers {
+                capturedSettings[firstArg()] = secondArg()
+                mockUpdate
+            }
+            every { mockUpdate.apply() } returns mockApp
+
+            val service = project.gradle.sharedServices.registerIfAbsent(
+                "functions-service",
+                MockFunctionAppClientBuildService::class
+            ) { spec -> spec.parameters.resourceGroup.set("test-rg") }
+
+            val params = project.objects.newInstance<UpdateFunctionAppSettingsAction.Parameters>()
+            params.appService.set(service)
+            params.appName.set("my-app")
+            params.settings.put("LOG_LEVEL", "info")
+            params.settings.put("REGION", "eastus")
+            params.sensitiveSettings.put("DB_CONNECTION_STRING", CredentialReference.Literal("secret-conn"))
+
+            val action = object : UpdateFunctionAppSettingsAction() {
+                override fun getParameters() = params
+            }
+            action.execute()
+
+            capturedSettings shouldContainAll mapOf(
+                "LOG_LEVEL" to "info",
+                "REGION" to "eastus",
+                "DB_CONNECTION_STRING" to "secret-conn",
+            )
+        }
+    }
+}
